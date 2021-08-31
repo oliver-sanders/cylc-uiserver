a96aa6557bfdff03508f1fe306e4391f56ee2890
Mel Hall <37735232+datamel@users.noreply.github.com>
2021-08-31 12:20:19 +0100

    Add configuration for Auth, including docs and add add authorization middleware

--- cylc/uiserver/jupyter_config.py
+++ cylc/uiserver/jupyter_config.py
@@ -15,6 +15,7 @@
 
 # Configuration file for jupyterhub.
 
+import os
 from pathlib import Path
 import pkg_resources
 
@@ -46,3 +47,7 @@ RUNTIME_PATH = Path('~/.cylc/hub').expanduser()
 c.JupyterHub.cookie_secret_file = f'{RUNTIME_PATH / "cookie_secret"}'
 c.JupyterHub.db_url = f'{RUNTIME_PATH / "jupyterhub.sqlite"}'
 c.ConfigurableHTTPProxy.pid_file = f'{RUNTIME_PATH / "jupyterhub-proxy.pid"}'
+
+# Ensure env variable CYLC_SITE_CONF_PATH is passed to UI Server environment
+c.Spawner.environment = {
+    'CYLC_SITE_CONF_PATH': f'{os.getenv("CYLC_SITE_CONF_PATH")}'}
